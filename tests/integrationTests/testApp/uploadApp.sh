743f70807666660b1ff0199f99f6b46b3ef1e421
Niv Raviv <niv.raviv@payu.com>
2022-12-27 10:33:19 +0200

    add yaml file for deploying srvices

--- tests/integrationTests/testApp/uploadApp.sh
+++ tests/integrationTests/testApp/uploadApp.sh
@@ -4,3 +4,6 @@ IMG=apptest:new
 cd ./tests/integrationTests/testApp/
 docker build -t ${IMG} -f ./appTest.Dockerfile .
 kind load docker-image ${IMG} --name ${CLUSTER_NAME}
+cd ../
+kubectl apply -f ./yamlFiles/services.yaml
+
